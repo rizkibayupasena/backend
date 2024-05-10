785b124cfdd979e5bfae7e43c5875d02096bd467
Rizky Bayy <rizkybayupasena18@gmail.com>
2024-05-10 19:58:57 +0700

    First Commit

---
+++ chatbot_logic.py
@@ -0,0 +1,13 @@
+
+import aiml
+
+class Chatbot:
+    def __init__(self):
+        # Inisialisasi AIML Interpreter
+        self.kernel = aiml.Kernel()
+        # Memuat file AIML
+        self.kernel.learn('chatbot.aiml')
+
+    def get_bot_response(self, user_input):
+        # Mengembalikan respons dari chatbot berdasarkan input pengguna
+        return self.kernel.respond(user_input)
---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from chatbot_logic import Chatbot
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+chatbot = Chatbot()
+CORS(app)
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    user_message = request.json['message']
+    bot_response = chatbot.get_bot_response(user_message)
+    return jsonify({'response': bot_response})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
