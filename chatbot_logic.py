ba999e324972647eaa4623286d5e656d33dfb7dc
Rizky Bayy <rizkybayupasena18@gmail.com>
2024-05-23 17:09:16 +0700

    update training aiml

--- chatbot_logic.py
+++ chatbot_logic.py
@@ -7,6 +7,7 @@ class Chatbot:
         self.kernel = aiml.Kernel()
         # Memuat file AIML
         self.kernel.learn('chatbot.aiml')
+        self.kernel.learn('training.aiml')
 
     def get_bot_response(self, user_input):
         # Mengembalikan respons dari chatbot berdasarkan input pengguna
